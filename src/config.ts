3097d206348b0bb324fea9e92d2829e787b35c35
Marilyn2022 <111874848+Marilyn2022@users.noreply.github.com>
2023-12-22 21:07:53 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 export const SITE = {
-  title: '潮流周刊',
-  author: 'Tw93',
-  description: '记录工程师 Tw93 的不枯燥生活，每周一发布，欢迎关注',
-  keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pack,Swift',
+  title: '网盘搜罗',
+  author: 'Marilyn',
+  description: '搜罗汇总迅雷、夸克、阿里网盘资源，欢迎关注',
+  keywords: 'Marilyn,wangpan,Blog,前端,macOS,MiaoYan,Pack,Swift',
   icon: 'https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png',
   pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
-  homePage: 'https://weekly.tw93.fun',
-  blogPage: 'https://tw93.fun/',
-  twitterId: 'HiTw93',
-  githubId: 'tw93',
-  repo: 'tw93/weekly'
+  homePage: 'https://daily.205066.xyz',
+  blogPage: 'https://www.205066.xyz/',
+  twitterId: '',
+  githubId: '',
+  repo: 'Marilyn2022/tweekly'
 };
 
