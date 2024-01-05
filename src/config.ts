3a8ee712e29a2415655d7a30c8107d0a62ff72fa
Marilyn2022 <111874848+Marilyn2022@users.noreply.github.com>
2024-01-05 10:45:42 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const SITE = {
   description: '搜罗汇总迅雷、夸克、阿里网盘资源，欢迎关注',
   keywords: 'Marilyn,wangpan,Blog,前端,macOS,MiaoYan,Pack,Swift',
   icon: 'https://www.205066.xyz/favicon.ico',
-  pic: 'https://www.205066.xyz/favicon.ico',
+  pic: 'https://img.imgdd.com/f210f3.9c736fd2-c6cd-4b31-ac46-cdf2d46f15e3.png',
   homePage: 'https://day.tsq360.cf',
   blogPage: 'https://www.205066.xyz/',
   twitterId: '',
